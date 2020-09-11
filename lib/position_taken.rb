80171a2d3db53d370f7f16db569f3bfd500fb2e1
selam lemma <oneiconforlife26@gmail.com>
2020-09-11 19:34:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken("board")
+  if(board == " ")
+    return "false"
+  elsif(board == "2"
+    return "true"
+  else
+    return postion is taken
+  end
+
+     puts position_taken
+
+
+def
